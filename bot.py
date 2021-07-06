2ec42d52ea1a09e1ff79c30d170d329e151ce0f5
DadsDuds <86989937+DadsDuds@users.noreply.github.com>
2021-07-05 21:33:26 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,67 @@
+import discord
+import os
+import json
+import requests
+import random
+
+client = discord.Client()
+
+global search_term_public 
+global url
+search_term_public = "dead chat"
+
+def tenor():
+    global url
+    # set the apikey 
+    apikey = (os.getenv("TENORAPIKEY"))
+
+    # our test search
+    search_term = search_term_public
+
+    # get the GIFs for the search term
+    r = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&contentfilter=high" % (search_term, apikey))
+
+    if r.status_code == 200:
+            # load the GIFs using the urls for the smaller GIF sizes
+            top_8gifs = json.loads(r.content)
+            g = len(top_8gifs['results'])
+            i = random.randint(0,g)
+            if(i == g):
+                    i = g-1
+            h = str(g)
+            f = str(i)
+            url = top_8gifs['results'][i]['media'][0]['gif']['url']
+            print("The number picked is " + f +" out of " + h + ". Search Term : " + search_term + ". Url : " +url)
+    else:
+            top_8gifs = None
+    return url
+@client.event
+async def on_ready():
+        await client.change_presence(status = discord.Status.online, activity = discord.Game('Injecting the White House Computer with a ZIP Bomb B)'))
+        print("Bot has successfully logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+        global search_term_public
+        if message.author == client:
+                return
+        if message.content.startswith("!deadchat"):
+                # put the search term into the public variable. split the content with space and the second or more than second word should be in a variable
+                tokens = message.content.split(' ')
+                if tokens.__contains__(""):
+                        tokens.remove("!deadchat")
+                        tokens.remove("")
+                elif tokens.__contains__("#"):
+                        token = token.replace("#", "%23")
+                else :
+                        tokens.remove("!deadchat")     
+                search_term_public =  ("".join(tokens))
+                if search_term_public == "":
+                        search_term_public = "dead chat"     
+                url = tenor()
+                await message.channel.send(url)
+
+
+
+
+client.run('ODMxMzQ3Njk3ODIyODU5MjY0.YHT64A.ZzliQj0mJTrV_CBj1yHRl1IPFD4')
